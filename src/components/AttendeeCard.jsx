b13f1f367e3ef8c5b961211e47ade96347ef6897
Gabbie-Darlin <gabdarlin@gmail.com>
2026-06-17 02:20:42 +0000

    Project init

---
+++ src/components/AttendeeCard.jsx
@@ -0,0 +1,40 @@
+function AttendeeCard(
+     {
+        AttendeeCard,
+        onToggleStatus,
+    }
+
+) {
+    return (
+        <div className="card">
+
+            <h3>{attendee.name}</h3>
+            <p>{attendee.role}</p>
+
+            <p>
+                Status:
+                {" "}
+                <strong>
+                    {
+                        attendee.registered 
+                        ? "Registered"
+                        : "Cancelled"
+                    }
+                </strong>
+            </p>
+
+            <button
+                onClick={() => onToggleStatus(attendee.id)}
+            >
+                {
+                    attendee.registered
+                    ? "Cancel Registration"
+                    : "Register Again"
+                }
+            </button>
+        </div>
+    );
+}
+
+export default AttendeeCard;
+   
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+        <header className="header">
+            <h1>Tech conference 2026</h1>
+            <p>
+                Manage conference registrations
+            </p>
+        </header>
+    );
+}
+
+export default Header;
